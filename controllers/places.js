a91a42073142c6747d8d76351986b4bd840fe3c6
tjsiv <tjsivelsjr@gmail.com>
2024-05-06 21:17:41 -0700

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,28 @@
 const router = require('express').Router()
 
+//get places new
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+
+//get places id
 router.get('/', (req, res) => {
     
     let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
+      name: 'H-Thai-ML',
+      city: 'Seattle',
+      state: 'WA',
+      cuisines: 'Thai, Pan-Asian',
+      pic: '/images/h-thai-ml-tables.jpg'
+    }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
+        pic: '/images/coffee-cat.jpg'
+    }]
+  
     res.render('places/index.jsx', { places })
 })
 
--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,8 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <img src="/images/er40.png" alt="error code 404" />
+                
           </main>
       </Def>
     )
