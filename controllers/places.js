9b12e74b67e7c157466166b8ac8626bb1e51fc39
tjsiv <tjsivelsjr@gmail.com>
2024-05-08 20:07:42 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,11 +1,25 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-//places first render
+//prop show route
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
+//places first render 
 router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
-
 
 //get places new
 router.get('/new', (req, res) => {
