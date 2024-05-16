0310d16a2283bbdf7513d9932be9f216961bd623
tjsiv <tjsivelsjr@gmail.com>
2024-05-15 21:26:22 -0700

    Added schema validation and seeds for demo

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,38 +5,47 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
-            <div className="col-sm-5">
-                <img src={data.place.pic} alt={data.place.name} />
-            </div>
-            <div className="col-sm-6">
-                <div>
-                    <h2>Rating</h2>
-                    <p>Not Rated</p>
-                </div>    
-                <div>
-                    <h2>Description</h2>
-                    <p className="text-center">
-                            Located in {data.place.city}, {data.place.state} and serving 
-                            {data.place.cuisines}  
-                    </p>
+            <div className="row">
+                <div className="col-sm-6">
+
+                    <img src={data.place.pic} alt={data.place.name} />
+                    <h3>
+                        Located in {data.place.city}, {data.place.state}
+                    </h3>
                 </div>
-                <div>
-                    <h2>Comments</h2>
-                    <p>No comments yet!</p>    
+                <div className="col-sm-6"> 
+                    <h1>{ data.place.name }</h1>
+                    <div>
+                        <h2>Rating</h2>
+                        <p>Not Rated</p>
+                    </div>             
+                    <div>
+                        <h2>Description</h2>
+                        
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                    </div>
+                    <div>
+                        <h2>Comments</h2>
+                        <p>No comments yet!</p>    
+                    </div>
+                    
                 </div>
-            </div>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                Edit
-            </a>     
- 
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">
-                    Delete
-                </button>
-            </form> 
-     
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                    Edit
+                </a>     
     
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form> 
+     
+            </div>
  
 
           </main>
