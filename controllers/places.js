144c4e3c32360c047a7efc1277691ea3555ad217
tjsiv <tjsivelsjr@gmail.com>
2024-05-18 14:36:21 -0700

    Added Comment functionality and seed data

--- controllers/places.js
+++ controllers/places.js
@@ -147,10 +147,11 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-//SHOW Route
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -158,6 +159,7 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
+
 
 
 
@@ -173,9 +175,30 @@ router.get('/:id/edit', (req, res) => {
   res.send('GET edit form stub')
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+//COMMENT POST ROUTE
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
+
+
+
 
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+          No comments yet!
+        </h3>
+      )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border">
+            <h2 className="rant">{c.rant ? 'Rant! 🤬😡' : 'Rave! 😍🤩'}</h2>
+            <h4>{c.content}</h4>
+             <h3>
+               <stong>- {c.author}</stong>
+             </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+         )
+       })
+    }
     return (
         <Def>
           <main>
@@ -28,22 +47,49 @@ function show (data) {
                         <h4>
                             Serving {data.place.cuisines}
                         </h4>
-                    </div>
-                    <div>
-                        <h2>Comments</h2>
-                        <p>No comments yet!</p>    
-                    </div>
-                    
-                </div>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                    Edit
-                </a>     
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                        Edit
+                    </a> 
+
     
                 <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
                     <button type="submit" className="btn btn-danger">
                         Delete
                     </button>
                 </form> 
+                    </div>
+                    <div>
+                    <hr />
+                        <h2>Comments</h2>
+                        {comments}
+
+                        <form method='POST' action={`/places/${data.place.id}/comment`}>
+
+                        <div className='form-group'>
+                            <label htmlFor='author'>Author</label>
+                            <input className='form-control' id='author' name='author'/>
+                        </div>
+
+                        <div className='form-group'>
+                            <label htmlFor='content'>Content</label>
+                            <input className='form-control' id='content' name='content' type='textarea' />
+                        </div>
+
+                        <div className='form-group'>
+                            <label htmlFor='stars'>Star Rating</label>
+                            <input className='form-control' id='stars' name='stars' type='range' step='0.5' min='0' max='5' />
+                        </div>
+
+                        <div className='form-group'>
+                            <label htmlFor='rant'>Rant</label>
+                            <input  id='rant' name='rant' type='checkbox' defaultChecked/>
+                        </div>
+
+                        <input className='btn btn-primary' type='submit' value='Add Comment' />
+                        </form>
+                    </div>
+                </div>
+
      
             </div>
  
