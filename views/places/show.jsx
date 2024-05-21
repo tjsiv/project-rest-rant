e44600079b3de5a9e46378d28cb4aaf634b32e98
tjsiv <tjsivelsjr@gmail.com>
2024-05-20 20:39:58 -0700

    added rating with stars

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,20 +7,37 @@ function show (data) {
           No comments yet!
         </h3>
       )
-    if (data.place.comments.length) {
-      comments = data.place.comments.map(c => {
-        return (
-          <div className="border">
-            <h2 className="rant">{c.rant ? 'Rant! 🤬😡' : 'Rave! 😍🤩'}</h2>
-            <h4>{c.content}</h4>
-             <h3>
-               <stong>- {c.author}</stong>
-             </h3>
-            <h4>Rating: {c.stars}</h4>
-          </div>
-         )
-       })
-    }
+      
+      let rating = (
+        <h3 className="inactive">
+          Not yet rated
+        </h3>
+      )
+      if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+          return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+          <h3>
+            {Math.round(averageRating)} ⭐
+          </h3>
+        )
+        }
+        // if (data.place.comments.length) {
+        //   comments = data.place.comments.map(c => {
+        //     return (
+        //       <div className="border">
+        //         <h2 className="rant">{c.rant ? 'Rant! 🤬😡' : 'Rave! 😍🤩'}</h2>
+        //         <h4>{c.content}</h4>
+        //          <h3>
+        //            <stong>- {c.author}</stong>
+        //          </h3>
+        //         <h4>Rating: {c.stars}</h4>
+        //       </div>
+        //      )
+        //    })
+        // }
     return (
         <Def>
           <main>
@@ -36,7 +53,8 @@ function show (data) {
                     <h1>{ data.place.name }</h1>
                     <div>
                         <h2>Rating</h2>
-                        <p>Not Rated</p>
+                        {rating}
+                        <br />
                     </div>             
                     <div>
                         <h2>Description</h2>
@@ -100,4 +118,3 @@ function show (data) {
 }
 
 module.exports = show
-
